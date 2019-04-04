9a88be236c70161e3f02e460df85c0d9f3ff93fe
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:44:23 -0300

    Delete desafio12.py

--- desafio12.py
+++
@@ -1,9 +0,0 @@
-# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
-
-p1 = float(input('Digite o preço '))
-
-r = (p1 * 5) / 100
-
-r2 = p1 - r
-
-print('o preço com 5% de desconto é {}'.format(r2))
