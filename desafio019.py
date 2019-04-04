37c8ea5c2b6ef102c7f150fa6377c4cfb71d07fe
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:51:33 -0300

    Delete desafio019.py

--- desafio019.py
+++
@@ -1,9 +0,0 @@
-# Um professor quer sortear um dos seus quantro alunos para apagar o quadro.
-# Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
-import random
-nome1 = input('Aluno 1 digite seu nome ')
-nome2 = input('Aluno 2 digite seu nome ')
-nome3 = input('Aluno 3 digite seu nome ')
-nome4 = input('Aluno 4 digite seu nome ')
-sera = random.rand
-print('O aluno que apagará o quandro será {}'.format(m))
