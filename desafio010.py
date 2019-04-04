eef2fd2fbcbf7d50b07c35904a24b39b5b08bec3
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:52:47 -0300

    Delete desafio010.py

--- desafio010.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dolares ela pode comprar
-#Considere que US$1,00 = R$3,27
-
-d1 = float(input('digite quanto tem na carteira '))
-
-r1 = d1 / 3.27
-
-print('voce pode comprar {:.2f} dolares'.format(r1))
