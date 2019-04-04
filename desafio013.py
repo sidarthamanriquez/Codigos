7ba9d0d54c5d73caf01cbf2261ca1da8663a7077
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:43:30 -0300

    Delete desafio013.py

--- desafio013.py
+++
@@ -1,9 +0,0 @@
-# Faça um algoritmo que leia o salário de um funcionário e mostre o seu novo salário, com 15% de aumento
-
-salarioAtual = float(input('Digite seu slário atual para saber o novo com aumento de 15% '))
-
-porcetagem15 = (salarioAtual * 15) / 100
-
-resultado = porcetagem15 + salarioAtual
-
-print('Seu sálario com 15% de aumto é {:.2f}'.format(resultado))
