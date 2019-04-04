f2c14a36a82d17f82fac29dafd37e59943f02abc
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:52:28 -0300

    Delete desafio014.py

--- desafio014.py
+++
@@ -1,7 +0,0 @@
-# crie um algoritmo que transforme a temperatura de C para F
-
-te1 = float(input('digite uma temperatura em graus '))
-
-re = ((te1 * 9) / 5) + 32  # se quiser não precisa dos parentesis
-
-print('a temperatura em Fahrenheit é {}'.format(re))
