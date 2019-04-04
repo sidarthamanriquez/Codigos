2c228094992da0bbec69c3c90d3915c4f1967c7f
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:44:15 -0300

    Delete desafio008.py

--- desafio008.py
+++
@@ -1,8 +0,0 @@
-#Escreva um programa que leia um valor em metros e o exiba em centimetros e milimetros
-
-m1 = float(input('Digite sua altura '))
-
-c = m1 * 100
-m = m1 * 1000
-
-print('o {} em centimetros {} e em milimetros {}'.format(m1, c, m))
