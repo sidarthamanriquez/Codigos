b2a1afe69382bd9ee4a20b17ba63fd94ffe1db8f
sidarthamanriquez <49203246+sidarthamanriquez@users.noreply.github.com>
2019-04-04 13:52:05 -0300

    Delete desafio016.py

--- desafio016.py
+++
@@ -1,7 +0,0 @@
-# crie um algoritmo que leia um nro Real qualquer pelo teclado e mostre na tela a sua porção Inteira
-# Ex: 'digite um nro ' 6.127
-# o nro 6.127 tem a parte inteira 6
-
-flutua = float(input('digite um nro Real '))
-
-print('O nro {} tem a parte inteira {:.0f}'.format(flutua, flutua))
